2f3bc13ccaf352c6a0d76f853ad99065843e3998
Shubham Vishwakarma <107599672+ShubhamVishwakarma11@users.noreply.github.com>
2022-08-18 18:15:21 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (32.76%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     
-    int f (int row, int col, vector<vector<int>> &grid) {
-        if (col == 0) return 1;
-        if (row == col) return 1;
-        if (grid[row][col] != 1) return grid[row][col];
-        return grid[row][col] = f(row-1,col-1,grid) + f(row-1,col,grid);
-    }
-    
     vector<vector<int>> generate(int n) {
         vector<vector<int>> grid;
         for (int i=1;i<=n;i++) {
@@ -15,8 +8,10 @@ public:
             grid.push_back(v);
         }
         
-        for (int i=0;i<n;i++) {
-            grid[n-1][i] = f(n-1,i,grid);
+        for (int row=2;row<n;row++) {
+            for (int col=1;col<row;col++) {
+                grid[row][col] = grid[row-1][col-1] + grid[row-1][col];
+            }
         }
         
         return grid;
