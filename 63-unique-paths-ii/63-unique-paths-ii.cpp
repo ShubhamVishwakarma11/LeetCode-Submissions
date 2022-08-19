3a5a307f3325b090604d0a34a5f0cebd03ac0c4d
Shubham Vishwakarma <107599672+ShubhamVishwakarma11@users.noreply.github.com>
2022-08-19 14:43:41 +0530

    Time: 7 ms (39.85%), Space: 7.6 MB (78.29%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -3,29 +3,28 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
-        vector<vector<int>> dp (n, vector<int> (m,-1));
+        vector<int> prev (m,0);
         
         if (obstacleGrid[0][0] == 1) {
-            dp[0][0]=0;
+            prev[0]=0;
         }
-        else dp[0][0] = 1;
+        else prev[0]= 1;
         
         for (int i=0;i<n;i++) {
+            vector<int> curr (m,-1);
             for (int j=0;j<m;j++) {
-                if (i==0 && j==0) continue;
-                else if (obstacleGrid[i][j] == 1) dp[i][j] = 0;
-                else if (i==0) {
-                    dp[i][j] = dp[i][j-1];
-                }
+                if (obstacleGrid[i][j] == 1) curr[j] = 0;
+                
                 else if (j==0) {
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
                 }
                 else {
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                    curr[j] = prev[j] + curr[j-1];
                 }
             }
+            prev = curr;
         }
         
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
