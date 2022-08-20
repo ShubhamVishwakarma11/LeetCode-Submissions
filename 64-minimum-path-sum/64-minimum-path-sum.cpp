d3d08d19b01b5b328ed47ce663eba68f11e49da8
Shubham Vishwakarma <107599672+ShubhamVishwakarma11@users.noreply.github.com>
2022-08-20 16:18:17 +0530

    Time: 10 ms (85.69%), Space: 10.1 MB (30.54%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    
-    int f (int row, int col, vector<vector<int>> &dp,vector<vector<int>> &grid) {
-        if (row == 0 && col == 0) {
-            return grid[0][0];
-        }
-        if (row<0 || col<0) return INT_MAX/2;
-        
-        if (dp[row][col] != -1) return dp[row][col];
-        
-        int up = f(row-1,col,dp,grid) + grid[row][col];
-        int left = f(row,col-1,dp,grid) + grid[row][col];
-        
-        return dp[row][col] = min(up,left);
-    }
-    
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp (n,vector<int> (m,-1));
+        vector<vector<int>> dp (n, vector<int> (m,-1));
+        dp[0][0] = grid[0][0];
         
-        return f(n-1,m-1,dp,grid);
+        for (int row=0;row<n;row++) {
+            for (int col=0;col<m;col++) {
+                if (row==0 && col == 0) {
+                    dp[row][col] = grid[row][col];
+                }
+                else  {
+                    int up = grid[row][col];
+                    int left = grid[row][col];
+                    if (row>0) up += dp[row-1][col];
+                    else up += INT_MAX/2;
+                    
+                    if (col>0) left += dp[row][col-1];
+                    else left += INT_MAX/2;
+                    
+                    dp[row][col] = min(up,left);
+                }
+            }
+        }
+        
+        return dp[n-1][m-1];
     }
 };
