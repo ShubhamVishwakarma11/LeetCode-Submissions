90f0c57d25a1406db92285899c720f87560e944c
Shubham Vishwakarma <107599672+ShubhamVishwakarma11@users.noreply.github.com>
2022-08-21 17:34:42 +0530

    Time: 17 ms (12.26%), Space: 8.7 MB (65.21%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp (n, vector<int> (n,INT_MIN));
+        vector<int> prev(n,INT_MIN);
         
         for (int row=0;row<n;row++) {
+            vector<int> curr (n,INT_MIN);
             for (int col=0;col<=row;col++) {
                 if (row==0) {
-                    dp[0][0] = triangle[0][0];
+                    curr[0] = triangle[0][0];
                 }
                 else if (col==0) {
-                    dp[row][col] = dp[row-1][col] + triangle[row][col];
+                    curr[col] = prev[col] + triangle[row][col];
                 }
                 else if (row == col) {
-                    dp[row][col] = dp[row-1][col-1] + triangle[row][col];
+                    curr[col] = prev[col-1] + triangle[row][col];
                 }
                 else {
-                    int left = dp[row-1][col-1] + triangle[row][col];
-                    int right = dp[row-1][col] + triangle[row][col];
-                    dp[row][col] = min (left,right); 
+                    int left = prev[col-1] + triangle[row][col];
+                    int right = prev[col] + triangle[row][col];
+                    curr[col] = min (left,right); 
                 }
             }
+            prev = curr;
         }
         
-        int res = dp[n-1][0];
-        for (auto it : dp[n-1]) {
+        int res = prev[0];
+        for (auto it : prev) {
             if (it<res) res = it;
         }
         
