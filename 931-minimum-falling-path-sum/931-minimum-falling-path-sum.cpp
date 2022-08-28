fb2003edbe7195012fd80cac478be3b2a7b2af10
Shubham Vishwakarma <107599672+ShubhamVishwakarma11@users.noreply.github.com>
2022-08-28 17:18:26 +0530

    Time: 18 ms (77.65%), Space: 10.3 MB (54.59%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -2,30 +2,32 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp (n, vector<int> (n,-101));
+        vector<int> prev (n,0);
         
         for (int row=0;row<n;row++) {
+            vector<int> dp (n,0);
             for (int col=0;col<n;col++) {
-                if (row==0){
-                    dp[0][col] = matrix[0][col];  
-                    continue;
-                } 
+                // if (row==0){
+                //     prev[col] = matrix[0][col];  
+                //     continue;
+                // } 
                 int ld = INT_MAX, rd = INT_MAX;
-                int up = dp[row-1][col];
+                int up = prev[col];
                 if (col!=0) {
-                    ld = dp[row-1][col-1];
+                    ld = prev[col-1];
                 }
                 if (col!=n-1) {
-                    rd = dp[row-1][col+1];
+                    rd = prev[col+1];
                 }
                 
-                dp[row][col] = matrix[row][col] + min(ld,min(up,rd));
+                dp[col] = matrix[row][col] + min(ld,min(up,rd));
             }
+            prev = dp;
         }
         
         int res = INT_MAX;
         for (int i=0;i<n;i++) {
-            res = min (res, dp[n-1][i]);
+            res = min (res, prev[i]);
         }
         
         return res;
